4e5b8ed58f13dc97789e32621c3cf090f2835d66
samarraly <s631998>
2019-04-04 21:05:41 +0200

    Thermonitor

--- app/src/main/java/guc/edu/thermonitor/DeviceDetailActivity.java
+++ app/src/main/java/guc/edu/thermonitor/DeviceDetailActivity.java
@@ -5,11 +5,15 @@ import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
@@ -25,20 +29,29 @@ public class DeviceDetailActivity extends AppCompatActivity {
    Button logout;
    FirebaseUser auth;
    ProgressDialog progrees;
-   private TextView MsgTxt;
-   private FirebaseDatabase firebaseDatabase=FirebaseDatabase.getInstance();
-   private DatabaseReference mRootReference = firebaseDatabase.getReference();
-   private DatabaseReference mChildResference = mRootReference.child("message");
+   private GoogleApiClient mGoogleSignInClient;
+   //private TextView MsgTxt;
+   private TextView celsius;
+   private TextView fahrenheit;
+   private FirebaseDatabase firebaseDatabase;
+   private DatabaseReference myRef;
+   //private DatabaseReference mChildResference = mRootReference.child("message");
+   String currentEsp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_device_detail);
+        Intent intent = getIntent();
+        currentEsp=intent.getStringExtra("name");
+        celsius = (TextView) findViewById(R.id.txtC);
+        fahrenheit = (TextView) findViewById(R.id.txtF);
         delete = (Button)findViewById(R.id.buttondelete);
         logout=(Button)findViewById(R.id.buttonlogout);
         auth= FirebaseAuth.getInstance().getCurrentUser();
-        MsgTxt=(TextView)findViewById(R.id.messsge);
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        myRef = firebaseDatabase.getReference();
         progrees=new ProgressDialog(this);
-        MsgTxt.setText("Message Appear here");
+
 
         delete.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -89,23 +102,55 @@ public class DeviceDetailActivity extends AppCompatActivity {
 
     }
 
+    //getting user
     @Override
     protected void onStart() {
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken("874551466482-tjvm46r9lsvnj1k5k4qefs8ocmj5po1r.apps.googleusercontent.com")
+                .requestEmail()
+                .build();
+        mGoogleSignInClient = new GoogleApiClient.Builder(this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+        mGoogleSignInClient.connect();
         super.onStart();
-        mChildResference.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                String message = dataSnapshot.getValue(String.class);
-                MsgTxt.setText(message);
+    }
 
+    public void readFromDatabase() {
+        // Read from the database
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                showData(dataSnapshot);
             }
 
             @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w("file", "Failed to read value.", error.toException());
             }
         });
+    }
 
-
+    private void showData(DataSnapshot dataSnapshot) {
+        for ( DataSnapshot ds : dataSnapshot.getChildren() ) {
+            if(currentEsp.equals("MyESPT")) {
+                Temperature temperature = new Temperature();
+                temperature.setCelsius(ds.child("MyESPT").getValue(Temperature.class).getCelsius());
+                temperature.setFahrenheit(ds.child("MyESPT").getValue(Temperature.class).getFahrenheit());
+                celsius.setText(temperature.getCelsius());
+                fahrenheit.setText(temperature.getFahrenheit());
+            }
+            else{
+                Temperature temperature = new Temperature();
+                temperature.setCelsius(ds.child("MyESPR").getValue(Temperature.class).getCelsius());
+                temperature.setFahrenheit(ds.child("MyESPR").getValue(Temperature.class).getFahrenheit());
+                celsius.setText(temperature.getCelsius());
+                fahrenheit.setText(temperature.getFahrenheit());
+            }
+            //tempList.setAdapter(tempAdapter);
+        }
     }
 }
