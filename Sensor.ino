a405911f3404c4f8677d32dbf6e2a6833d764ccf
Samar Aly <47306073+samarraly@users.noreply.github.com>
2019-06-04 21:04:40 +0200

    Add files via upload

---
+++ Sensor.ino
@@ -0,0 +1,107 @@
+/*
+   Copyright (c) 2015, Majenko Technologies
+   All rights reserved.
+
+   Redistribution and use in source and binary forms, with or without modification,
+   are permitted provided that the following conditions are met:
+
+ * * Redistributions of source code must retain the above copyright notice, this
+     list of conditions and the following disclaimer.
+
+ * * Redistributions in binary form must reproduce the above copyright notice, this
+     list of conditions and the following disclaimer in the documentation and/or
+     other materials provided with the distribution.
+
+ * * Neither the name of Majenko Technologies nor the names of its
+     contributors may be used to endorse or promote products derived from
+     this software without specific prior written permission.
+
+   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+   ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+   WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+   DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+   ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+   (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+   LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+   ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+   (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+   SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+/* Create a WiFi access point and provide a web server on it. */
+
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+#include <FirebaseArduino.h>
+#include <ArduinoJson.h>
+
+/* Set these to your desired credentials. */
+const char *ssid = "MyESPT";
+const char *password = "";
+#define FIREBASE_HOST "thermonitor-372c8.firebaseio.com"
+#define FIREBASE_AUTH "uXirzLX0a8Ea6KzBxLUnlBrzBISVtppEZtJPd3f2"
+#define WIFI_SSID "Helal"
+#define WIFI_PASSWORD "01222149078"
+
+
+int outputpin= A0;
+
+ESP8266WebServer server(80);
+
+/* Just a little test message.  Go to http://192.168.4.1 in a web browser
+   connected to this access point to see it.
+*/
+void handleRoot() {
+  server.send(200, "text/html", "<h1>You are connected</h1>");
+}
+
+void setup() {
+  //delay(1000);
+  Serial.begin(9600);
+  delay(1000);
+  Serial.println();
+  // connect to wifi.
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.println("Connecting to wifi");
+  Serial.println("Access point...");
+
+  
+  /* You can remove the password parameter if you want the AP to be open. */
+  WiFi.softAP(ssid, password);
+
+  IPAddress myIP = WiFi.softAPIP();
+  Serial.print("AP IP address: ");
+  Serial.println(myIP);
+  Serial.print("Server MAC address: ");
+  Serial.println(WiFi.softAPmacAddress());
+  server.on("/", handleRoot);
+  server.begin();
+  Serial.println("HTTP server started");
+
+  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);             //connect to Database
+  delay(1000);
+}
+
+void loop() {
+  server.handleClient();
+
+  int analogValue = analogRead(outputpin);
+  float millivolts = (analogValue/1024.0) * 3300; //3300 is the voltage provided by NodeMCU
+  float celsius = millivolts/10;
+  float fahrenheit = ((celsius * 9)/5 + 32);
+   
+  // set string value
+  String mac=WiFi.softAPmacAddress();
+  String c=String(celsius);
+  String f=String(fahrenheit);
+  Firebase.setString("/Temperature/MyESPT/celsius",c);
+  Firebase.setString("/Temperature/MyESPT/fahrenheit",f);
+  // handle error
+  if (Firebase.failed()) {
+      Serial.print("setting /message failed:");
+      Serial.println(Firebase.error());  
+      return;
+  }
+  delay(180000);
+}
